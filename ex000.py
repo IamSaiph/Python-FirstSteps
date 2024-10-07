44310aee5395d444e8cf96e284e928e2b6f98dd3
Pietra Estrela <pietraestrelaofice@gmail.com>
2024-10-07 16:13:56 -0300

    Enviei!!!

---
+++ ex000.py
@@ -0,0 +1,6 @@
+nome = str(input('Me diz seu nome, anjo? '))
+n1 = int(input('Agora poderia me dizer um número? '))
+n2 = int(input('Agora poderia me dizer outro número? '))
+s = n1 + n2
+
+print('{}, você me informou os números {} e {}. A soma deles equivale à: {} ' .format(nome, n1, n2, s))
